82cb46e129eb97fb7bd9c42cb9dbafaa9c279944
Alton Michaux <michauxboyz@yahoo.com>
2020-12-25 01:05:23 +0000

    still working on nav menu. Can't get it to disappear from page when not active. Did manage to clean menu up by getting rid of background and underlines from links.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// Greeting function that asks the user's name and if they like music,
-//depending on their answer, the function returns a conditional response.
 
 let show = () => {
   document.getElementById('sidebar').classList.toggle('active');
