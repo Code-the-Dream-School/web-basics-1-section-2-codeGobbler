12717fe073c1d169cac95d6656774e05076b0f98
Alton Michaux <alton.michaux@yahoo.com>
2020-12-27 19:49:16 -0500

    final commit to master branch before creating project branch to work on projects directory

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-let show = () => {
-  document.getElementById('sidebar').classList.toggle('active');
-  document.getElementById('btn').classList.toggle('active');
-}
+let sideBar = document.getElementById('sidebar');
+let toggleButton = document.getElementById('btn');
+
+sideBar.addEventListener('click', () => {
+  sideBar.classList.toggle('active');
+})
+
+toggleButton.addEventListener('click', () => {
+  toggleButton.classList.toggle('active');
+})
