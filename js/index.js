02290550ac915701a4d0a106e2c7cd106a18fdff
Alton Michaux <alton.michaux@yahoo.com>
2020-12-29 16:44:14 -0500

    formatted style.css and changed unused js id's to class names to save page resources for js

--- js/index.js
+++ js/index.js
@@ -3,16 +3,20 @@ const toggleButton = document.getElementById('btn');
 const projectGrid = document.getElementsByClassName('project-grid');
 console.log(projectGrid)
 
+
 sideBar.addEventListener('click', () => {
   sideBar.classList.toggle('active');
-})
-
+});
+  
 toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('active');
-})
+});
 
-projectGrid.addEventListener('mouseover', (event) => {
-  if (event.target.tagName == 'CANVAS') {
-    event.target.style.display = 'none';
-  }
-})
+
+const grow = () => {
+  projectGrid.addEventListener('mouseover', (event) => {
+    if (event.target.classList == 'project-block') {
+      event.target.style.display = 'none';
+    }
+  });
+}
