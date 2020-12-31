4b291979ee29660a66c90c5f5b640e7f64e303a1
Alton Michaux <alton.michaux@yahoo.com>
2020-12-30 19:08:52 -0500

    added project grid images and placed them within the canvas divs

--- js/index.js
+++ js/index.js
@@ -1,22 +1,59 @@
-const sideBar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('btn');
-const projectGrid = document.getElementsByClassName('project-grid');
-console.log(projectGrid)
+/*===============================================*/
 
+/*animate side menu*/
 
-sideBar.addEventListener('click', () => {
-  sideBar.classList.toggle('active');
+//variables to select side menu items and button
+const sideBar = document.getElementById("sidebar");
+const toggleButton = document.getElementById("btn");
+
+//variable to select project grid
+const projectGrid = document.getElementsByClassName("project-grid");
+
+/*Side menu button*/
+sideBar.addEventListener("click", () => {
+  sideBar.classList.toggle("active");
 });
-  
-toggleButton.addEventListener('click', () => {
-  toggleButton.classList.toggle('active');
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
 });
 
+/*===============================================*/
 
-const grow = () => {
-  projectGrid.addEventListener('mouseover', (event) => {
-    if (event.target.classList == 'project-block') {
-      event.target.style.display = 'none';
-    }
-  });
+/*draw images on canvas*/
+
+//variables to select canvas elements and declare context
+
+//declare image variables
+const image = new Image() 
+image.src="images/playlist_Website.png"
+
+const image2 = new Image() 
+image2.src="images/breakout_Game.png"
+
+const image3 = new Image() 
+image3.src="images/spotify_Clone.png"
+
+const image4 = new Image() 
+image4.src="images/battleship_Game.png"
+
+//create image object to store information for later use
+const img = [
+  image,
+  image2,
+  image3,
+  image4
+]
+
+//loop to place images in all canvases
+for (let i = 0; i < img.length; i++) {
+  console.log(img.length);
+  let canvas = document.getElementsByClassName('canvas')[i];
+  console.log(canvas);
+  let myContext = canvas.getContext('2d');
+  console.log(myContext);
+  img[i].onload = () => {
+    myContext.drawImage(img[i], 0, 0, 1300, 1300, 0, 0, 300, 336);
+  }
 }
+
